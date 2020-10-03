2709b4e994a11c13114497453f6ac865ec9c68d6
Piotrus Watson <piotruswatson@gmail.com>
2020-10-03 14:55:38 +0100

    added a thing

---
+++ Assets/BackgroundCreator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCreator : MonoBehaviour
+{
+    public Vector2Int startPoint;
+    public int numberOfTiles;
+    public GameObject[] backgroundTiles;
+    Grid grid; 
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        grid = GameObject.Find("Grid").GetComponent<Grid>();
+        mainCamera = Camera.main;
+
+        for (int i = 0; i < numberOfTiles; i++){
+            GameObject tile = SelectTile();
+            
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    GameObject SelectTile(){
+        int selected = Random.Range(0, backgroundTiles.Length);
+        return backgroundTiles[selected];
+    }
+}
