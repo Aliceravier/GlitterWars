672d3d8bc3a1942b8013ff9fe4254b4e8e8b677c
2206245r <2206245r@student.gla.ac.uk>
2020-10-03 12:26:00 +0100

    Added grid UI a bit, changed behaviour

---
+++ Assets/unit_behaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class unit_behaviour : NetworkBehaviour
+{
+    enum Status
+    {
+        Available,
+        Moved,
+        Shot
+    }
+    private Status status;
+
+    public int lengthOfMovement;
+    public List<int> directionsOfMovement; //ints 0 -> 7 indicating all cardinal directions
+    public List<int> directionsOfShot;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            //show clickable grid UI of diameter lengthOfMovement which expands in directionsOfMovement
+            //get next click on grid and register movement command, change status to Moved
+
+            Debug.Log("Pressed primary button.");
+    }
+}
