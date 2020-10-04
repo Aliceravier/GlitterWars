0f7c2f7edd244376059cee521b12f6ee9762599c
2206245r <2206245r@student.gla.ac.uk>
2020-10-04 04:18:42 +0100

    Added more box

---
+++ Assets/box_behaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box_behaviour : MonoBehaviour
+{
+    public Sprite wholeBox;
+    public Sprite destroyedBox;
+    public GameObject cat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void destroyBox()
+    {
+        SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = destroyedBox;
+        //instantiate cat
+        Instantiate(cat);
+        Destroy(gameObject);
+    }
+}
